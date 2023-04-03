f06e97c91e23a49ee798a82912617839f7e0e499
Luke Boydstun <luke_boydstun1@baylor.edu>
2023-04-03 14:51:18 -0500

    update

--- 1_python_repos_start_file.py
+++ 1_python_repos_start_file.py
@@ -4,4 +4,4 @@ import requests
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 headers = {'Accept': 'application/vnd.github.v3+json'}
 r = requests.get(url, headers=headers)
-print(f"Status code: {r.status_code}")
+print(f"Status code: {r.status_code}") 
